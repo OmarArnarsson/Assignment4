3a2889e89a6fb4bd383bea9e6317c8b3ff620774
h0gni <hfg7@hi.is>
2019-04-09 00:21:33 +0000

    bætti setterum við bookingmain classan

--- JavaFXApplication4/src/is/hi/assignment4/Bookingmain.java
+++ JavaFXApplication4/src/is/hi/assignment4/Bookingmain.java
@@ -8,7 +8,7 @@ package is.hi.assignment4;
 import java.util.Calendar;
 import modeldaytour.BookingD;
 import modeldaytour.Customer;
-import  modelflight.BookingF;
+import modelflight.ConnectedBooking;
 import modelflight.Traveller;
 /**
  *
@@ -20,13 +20,13 @@ public class Bookingmain {
     
    // private BookingID
     private Package pack;
-    private BookingF flightBooking1;    
-    private BookingF flightBooking2;
+    private ConnectedBooking flightBooking1;    
+    private ConnectedBooking flightBooking2;
     private BookingD dtBooking;
     private Traveller traveller;
     private Customer customer;
     //hlutir sem við fáum frá interface-inu
-    private Integer costumerID;
+    private String costumerID;
     private String seatnumber;
     private Integer baggage;
     private String firstName;
@@ -35,6 +35,7 @@ public class Bookingmain {
     private Calendar birthday;
     private String passportNumber;
     private String phoneNumber;
+    private String nationality;
     
 
 
@@ -44,7 +45,40 @@ public class Bookingmain {
     }
     
     public void makeBookings(){
-        this.flightBooking1 = new BookingF(this.pack.f1, this.seatnumber, this.baggage, this.pack.f1.getTotalEconomyPrice());
+        this.flightBooking1 = new ConnectedBooking(this.pack.f1);
+        this.flightBooking2 = new ConnectedBooking(this.pack.f2);
+        this.traveller= new Traveller(this.firstName, this.birthday, this.nationality,this.passportNumber, this.email, this.phoneNumber);
     }
 
+    
+    public void setCostumerIDsetter(String ID){
+        this.costumerID=ID;
+    }
+    public void setSeatNumber(String seat){
+        this.seatnumber=seat;
+    }
+    public void setBaggage(Integer baggage){
+        this.baggage=baggage;
+    }
+    public void setFirstName(String name){
+        this.firstName=name;
+    }
+    public void setLastName(String name){
+        this.lastName=name;
+    }
+    public void setemail(String email){
+        this.email=email;
+    }
+    public void setBirthDay(Calendar birthd){
+        this.birthday=birthd;
+    }
+    public void setPassportNumber(String pass){
+        this.passportNumber=pass;
+    }
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber=phoneNumber;
+    }
+    public void setNationality(String nationality){
+        this.nationality=nationality;
+    }
 }
